bd20f382819addf27abb52e6abc1256a75c70468
redsonola@gmail.com <redsonola@gmail.com>
2024-10-01 02:37:31 -0500

    added polyphonic and multiple parts to midi capabilities

--- src/main/java/com/example/MelodyPlayer.java
+++ src/main/java/com/example/MelodyPlayer.java
@@ -2,7 +2,7 @@
  * c2017-c2023 Courtney Brown 
  * 
  * Class: MelodyPlayer
- * Description: Sends a melody of midi notes to an external player/midi channel
+ * Description: Sends a melody of midi notes to an external player/midi channel, revised 2024 for polyphonic playing
  * 
  */
 
@@ -22,17 +22,24 @@ public class MelodyPlayer {
 	float bpm; // beats per minute of melody
 
 	double start_time; //time at creation of the melody player in milliseconds
+	double playStart = 0;
 
 	double rhythm_multiplier; //determines note length and onset of the next note
 
 	ArrayList<Integer> melody; //list of pitches in order
 	ArrayList<Double> rhythm; //list of note lengths (& thus time before next note, in order)
+	ArrayList<Double> startTimes; //list of start times
 
 	boolean hasRhythm; //has there been a list of rhythms assigned?
 	boolean hasMelody; //has there been a list of pitches assigned?
-	boolean lastNoteOff = false; //have we sent the last note off?
+	boolean hasStart = false; 
 
 	String outputBus; //bus to send MIDI to -- change if you have named it something else or you are using Windows
+
+	//list of playing notes to keep track off for note offs
+	ArrayList<Double> playingRhythms = new ArrayList();
+	ArrayList<Double> playingTimes = new ArrayList();
+	ArrayList<Integer> playingPitches = new ArrayList(); 
 
 	
 
@@ -59,9 +66,14 @@ public class MelodyPlayer {
 		hasRhythm = true;
 	}
 
+	void setStartTimes(ArrayList<Double> r) {
+		startTimes = r;
+		hasStart = true;
+	}
+
 	// display all ports available to the MidiBus -- only output ports are relevant, however
 	//if OS X, best to choose the IAC bus we created (for mac/OS X) so can send to an external program (eg DAW/sampler)
-	//if Windows, why are you LIKE this??! -- TODO: install/document virtual port on Windows
+	//if Windows, why are you LIKE this??! -- TODO: install/document virtual port on Windows via 3rd party software
 	void listDevices()
 	{
 		MidiBusCRCP.listDevices(); 
@@ -83,86 +95,102 @@ public class MelodyPlayer {
 		double millisNow = System.currentTimeMillis()-start_time;
 		return (int) millisNow;
 	}
-	
+
 	//send the melody out in MIDI messages, in correct timing, to the external program
-	void play() {
+	void play() 
+	{
+		int vel = 100; //midi velocity -- TODO: change/assign if want to vary
+		double cur_time = millis(); //what time is it now?
+
+		//send note off messages for any notes that have been playing
+		sendNoteOff(cur_time);
+
 		// just do nothing if there is no melody (pitches)
 		if (!hasMelody)
 		{
 			System.out.println("There is no melody in the notes given.");
 			return;
 		}
-
-		//if we're at the end of the melody
+	
+		//if we're at the end of the melody also do nothing
 		if(atEndOfMelody())
 		{
 			return;
 		}
 
-		int vel = 100; //midi velocity -- TODO: change/assign if want to vary
-		float cur_time = millis(); //what time is it now?
-		play = cur_time - last_time >= notems * rhythm_multiplier; //should we send the note now? based on prev. note's duration
+		//playing the notes by sending the note ons at the corect times
+		if( note_index < startTimes.size() )
+		{
+			play = cur_time - playStart >= notems * startTimes.get(note_index); //should we send the note now? based on prev. note's duration
 
-		if (play)
-			last_time = cur_time;
+			//send note on messages
+			if( play && note_index < startTimes.size() ) {
+				sendNoteOn(note_index, cur_time, vel);
+				note_index++;
+				boolean sameTime = true;
 
-		//send a note off to previous note -- TODO: control note-offs on staccato or legato, etc. values
-		if (note_index <= melody.size() && note_index > 0 && play) {
-			outputMidiBus.sendNoteOff(0, (int) melody.get(note_index - 1), 0);
-			//System.out.println("note off:" + (note_index - 1)); //TODO: comment out when not debugging or not needed
-
-			// don't send anything else if done
-			if (note_index == melody.size() && lastNoteOff )
-				note_index = 0;// ++; //cycle vs. stop at end ? TODO: create as an option
-			else if (note_index == melody.size() && !lastNoteOff) //need to send the last note off before stopping, though, otherwise it hangs....
-			{
-				lastNoteOff = true; 
+				//send note-on messages for the notes that occur at the same time as well (eg. a chord, a dyad)
+				while( note_index < startTimes.size() && sameTime )
+				{
+					sameTime = (startTimes.get(note_index-1).equals(startTimes.get(note_index)));
+					if( sameTime )
+					{
+						sendNoteOn(note_index, cur_time, vel);
+						note_index++;
+					}
+				}
 			}
-
 		}
-		
-		//send out next pitch, find next rhythm / duration
-		if (note_index < melody.size() && note_index > -1 && play) {
-
-			outputMidiBus.sendNoteOn(0, (int) melody.get(note_index), vel);
-			//System.out.println("note on:" + note_index); //TODO: comment out when not debugging or not needed
-			//get -- if its a noteOn, & what note
-			if (hasRhythm)
-				rhythm_multiplier = rhythm.get(note_index);
-
-			note_index++;
-
-			//this is tail-end recursion so could be optimized into a for-loop but TODO
-			if( note_index+1 < rhythm.size()-1)
-			{
-				rhythm_multiplier = rhythm.get(note_index+1);
-				play = cur_time - last_time >= notems * rhythm_multiplier; //should we send the note now? based on prev. note's duration
-				if(play) { play(); }
-			}
-
 			
+	}
+
+	//send a note on message and then add to playing note arrays
+	void sendNoteOn(int note_index, double cur_time, int vel)
+	{
+		outputMidiBus.sendNoteOn(0, (int) melody.get(note_index), vel);
+		playingRhythms.add( rhythm.get(note_index));
+		playingTimes.add( cur_time );
+		playingPitches.add(melody.get(note_index));
+	}
+
+	//send note off messages for the playing notes in our buffers
+	void sendNoteOff(double cur_time)
+	{
+		for(int i=playingRhythms.size()-1; i>=0; i-- )
+		{
+			if( playingRhythms.get(i) * notems  <= cur_time - playingTimes.get(i)  )
+			{					
+				outputMidiBus.sendNoteOff(0, (int) playingPitches.get(i), 0);
+				playingRhythms.remove(i);
+				playingPitches.remove(i);
+				playingTimes.remove(i);				
+			}
 		}
 	}
 	
 	//reset note to 0
 	void reset() {
 		note_index = 0;
-		lastNoteOff = false;
+		playStart = millis(); 
 
-		System.out.println("Reset called!!");
-		System.out.println(new Error().getStackTrace().toString());
+		playingRhythms.clear();
+		playingPitches.clear();
+		playingTimes.clear();
 	}
 
 	//set to ends
 	void setToEnd()
 	{
 		note_index = melody.size(); 
-		lastNoteOff = true;
+
+		playingRhythms.clear();
+		playingPitches.clear();
+		playingTimes.clear();
 	}
 
 	//have we reached the end of the melody?
 	boolean atEndOfMelody()
 	{
-		return note_index >= melody.size() && lastNoteOff;
+		return note_index >= melody.size();
 	}
 }
--- src/main/java/com/example/MidiFileToNotes.java
+++ src/main/java/com/example/MidiFileToNotes.java
@@ -20,7 +20,8 @@
 	 ArrayList<Double> rhythms; //the rhythmic values (in jm notation)
 	 ArrayList<Double> startTimes; //start times 
  
-	 int whichLine; //which instrument, basically -- but "line of music" or midi channel of music to be the most precise
+	 int whichLine = 0; //which instrument, basically -- but "line of music" or midi channel of music to be the most precise
+	 boolean setSingleLine = false;
  
 	 ArrayList<jm.music.data.Note> melody; //list of notes in jm form --> melody + rhythm
  
@@ -29,7 +30,6 @@
 	 MidiFileToNotes(String f) {
 		 filename = f;
 		 processPitchesAsTokens();
-		 whichLine = 0;
 	 }
  
 	 //set which line or instrument of the midi file
@@ -37,47 +37,75 @@
 	 void setWhichLine(int line) {
 		 whichLine = line;
 	 }
+
+	 //only read in one part of the score, the one indicated
+	 void setSingleLine(int index)
+	 {
+		whichLine = index;
+		setSingleLine = true;
+	 }
+
+	 //read in all parts of the score
+	 void setToAllParts()
+	 {
+		setSingleLine = false;
+	 }
+
  
-	 //extract the midi data from jm.music format into separate arrays of pitch and melody
+	 //extract the midi data from jm.music format into separate arrays of pitch and melody and startTimes
 	 void processPitchesAsTokens() {
+
+		//the pitches, melody, rhythms, and start times of all the notes
 		 pitches = new ArrayList<Integer>();
 		 melody = new ArrayList<jm.music.data.Note>();
 		 rhythms = new ArrayList<Double>();
 		 startTimes = new ArrayList<Double>();
  
+		 //score names for JMusic
 		 String scoreName = "score_" + filename;
 		 Score theScore = new Score(scoreName);
  
 		 // read the midi file into a score
 		 Read.midi(theScore, filename);
  
-		 // extract the melody and all its parts
-		 Part part = theScore.getPart(whichLine);
-		 Phrase[] phrases = part.getPhraseArray();
- 
-		 // extract all the pitches and notes from the melody & put them in MIDI style-note ordering using an insertion-style sort
-		 for (int i = 0; i < phrases.length; i++) {
-			 jm.music.data.Note[] notes = phrases[i].getNoteArray();
-			 for (int j = 0; j < notes.length; j++) {
- 
-				 int placeToAdd = rhythms.size(); //default, add at end
-				 for( int k=0; k<startTimes.size(); k++ )
-				 {
-					 if( startTimes.get(k) == phrases[i].getNoteStartTime(j)  )
-					 {
-						 rhythms.set(k, 0.0); 
-						 placeToAdd = k+1; 
-					 }
-				 }
-				 pitches.add(placeToAdd, notes[j].getPitch());
-				 rhythms.add(placeToAdd, notes[j].getRhythmValue() );
-				 startTimes.add(placeToAdd, phrases[i].getNoteStartTime(j));
-				 melody.add(placeToAdd, notes[j]);
-			 }
+		 // extract the melody and all its parts, unless requested a single line
+		 int partStart = 0;
+		 int partEnd = theScore.getPartArray().length;
+		 if(setSingleLine)
+		 {
+			partStart = whichLine-1; 
+			partEnd = whichLine;
 		 }
+
+		 //process the score here
+		 for(int whichPart=partStart; whichPart<partEnd; whichPart++)
+		 {
+			Part part = theScore.getPart(whichPart);
+			Phrase[] phrases = part.getPhraseArray();
+	
+			// extract all the pitches and notes from the melody & put them in MIDI style-note-ish ordering using an insertion-style sort
+			for (int i = 0; i < phrases.length; i++) {
+				jm.music.data.Note[] notes = phrases[i].getNoteArray();
+				for (int j = 0; j < notes.length; j++) {
+					
+					int placeToAdd =0;
+					boolean added = false;
+					while ( placeToAdd < startTimes.size() && !added )
+					{
+						added = phrases[i].getNoteStartTime(j) < startTimes.get(placeToAdd);
+						placeToAdd++;
+					}
+
+					if( added ) placeToAdd--;
+
+					pitches.add(placeToAdd, notes[j].getPitch());
+					rhythms.add(placeToAdd, notes[j].getDuration() );
+					startTimes.add(placeToAdd, phrases[i].getNoteStartTime(j));
+					melody.add(placeToAdd, notes[j]);
+				}
+			}
+		}
 	 }
- 
-	 
  
 	 public Integer[] getPitches() {
 		 return pitches.toArray(new Integer[pitches.size()]);
@@ -90,6 +118,10 @@
 	 public ArrayList<Double> getRhythmArray() {
 		 return rhythms;
 	 }
+
+	 public ArrayList<Double> getStartTimeArray() {
+		return startTimes;
+	}
  
 	 public ArrayList<jm.music.data.Note> getMelody() {
 		 return melody;
